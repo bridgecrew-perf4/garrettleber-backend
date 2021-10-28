d62cd8cfc3edcc51c4e86de28df1b4b5aaf907cd
lebergarrett <lebergarrett@gmail.com>
2021-10-28 16:48:12 +0000

    big changes, imported all remaining resources for website backend and broke into separate tf files

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 This repo holds the backend code for my personal website, created as part of the cloud resume challenge located at <https://cloudresumechallenge.dev>
 
-I chose to use terraform because I already have a slight familiarity with the tool.
+I chose to use terraform because I already have familiarity with the tool.
 
-This uses the packaged Python lambda function included in the src/ and package/ dirs, spins it up in AWS and configures the proper role to give it the permissions it needs. It then creates the REST API used by the function, and spits that out as output to be used in the frontend. At the end it creates the necessary DynamoDB table that stores the data for the visitor counter.
+This uses the packaged Python lambda function included in the src/ dir, spins it up in AWS and configures the proper role to give it the permissions it needs. It then creates the REST API used by the function, and spits that out as output to be used in the frontend. At the end it creates the necessary DynamoDB table that stores the data for the visitor counter.
 
 I also use Github Actions to create a pipeline that runs unit tests for the lambda function, and deploys the infrastructure if the tests pass. It only does this when pull requests are merged with master. When a pull request is created it runs everything except the apply, and shows the output of `terraform plan` in the pull request.
--- main.tf
+++ main.tf
@@ -1,44 +1,64 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
+locals {
+  site_domain = "garrettleber.com"
+  s3_origin_id = "S3-garrettleber.com"
+}
 
-  backend "s3" {
-    bucket = "garrettleber-tf-backend"
-    key    = "prod/terraform.tfstate"
-    region = "us-east-1"
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = local.site_domain
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+resource "aws_s3_bucket_policy" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Id        = "PolicyForCloudFrontPrivateContent"
+    Statement = [{
+      Action    = "s3:GetObject"
+      Effect    = "Allow"
+      Principal = {
+        AWS = aws_cloudfront_origin_access_identity.main.iam_arn
+      }
+      Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      Sid       = "1"
+    },]
+    Version   = "2008-10-17"
+  })
+}
+
+resource "aws_route53_zone" "website_zone" {
+  name = local.site_domain
+}
+
+resource "aws_route53_record" "main" {
+  zone_id = aws_route53_zone.website_zone.zone_id
+  name    = local.site_domain
+  type    = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.main.domain_name
+    zone_id = aws_cloudfront_distribution.main.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_cloudfront_distribution" "main" {
-
-  aliases = ["garrettleber.com"]
+  enabled = true
+  aliases = [aws_s3_bucket.website_bucket.bucket]
   default_root_object = "index.html"
   is_ipv6_enabled = true
+
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    compress               = false
-    default_ttl            = 0
-    max_ttl                = 0
-    min_ttl                = 0
-    smooth_streaming       = false
-    target_origin_id       = "S3-garrettleber.com"
-    trusted_signers        = []
+    default_ttl = 0
+    max_ttl = 0
+    target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "redirect-to-https"
-
     forwarded_values {
-        headers                 = []
         query_string            = false
-        query_string_cache_keys = []
-
         cookies {
             forward           = "none"
             whitelisted_names = []
@@ -46,166 +66,37 @@ resource "aws_cloudfront_distribution" "main" {
     }
   }
 
-  enabled = true
   origin {
-    domain_name = "garrettleber.com.s3.amazonaws.com"
-    origin_id   = "S3-garrettleber.com"
+    domain_name = aws_s3_bucket.website_bucket.bucket_domain_name
+    origin_id   = local.s3_origin_id
 
     s3_origin_config {
-        origin_access_identity = "origin-access-identity/cloudfront/E1AVFV6RD571KV"
+        origin_access_identity = aws_cloudfront_origin_access_identity.main.cloudfront_access_identity_path
     }
   }
 
   restrictions {
     geo_restriction {
       restriction_type = "none"
-      locations        = []
     }
   }
   
   viewer_certificate {
-    acm_certificate_arn            = "arn:aws:acm:us-east-1:327910803467:certificate/39970793-e6ed-4259-adbf-f69b02740c73"
-    cloudfront_default_certificate = false
+    acm_certificate_arn            = aws_acm_certificate.website_certificate.arn
     minimum_protocol_version       = "TLSv1.2_2019"
     ssl_support_method             = "sni-only"
   }
 }
 
-resource "aws_iam_role_policy" "lambda_cust_policy" {
-  name = "lambda_cust_policy"
-  role = aws_iam_role.lambda_cust_role.id
-
-  policy = jsonencode({
-    Version : "2012-10-17"
-    Statement : [
-      {
-        Action : "dynamodb:UpdateItem"
-        Effect : "Allow"
-        Sid : ""
-        Resource : "${aws_dynamodb_table.visitors_app_table.arn}"
-      },
-    ]
-  })
+resource "aws_cloudfront_origin_access_identity" "main" {
+  comment = "OAI to allow CloudFront distribution access to website S3 Bucket"
 }
 
-resource "aws_iam_role" "lambda_cust_role" {
-  name = "lambda_cust_role"
-
-  assume_role_policy = jsonencode({
-    Version : "2012-10-17"
-    Statement : [
-      {
-        Action : "sts:AssumeRole"
-        Effect : "Allow"
-        Sid : ""
-        Principal : {
-          "Service" : "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-data "aws_s3_bucket_object" "visitorsapp_function_hash" {
-  bucket = "garrettleber-tf-backend"
-  key    = "prod/lambda-zips/lambda_function_payload.zip.base64sha256"
-}
-
-resource "aws_lambda_function" "visitorsapp" {
-  function_name = "visitorsapp"
-  role          = aws_iam_role.lambda_cust_role.arn
-  handler       = "main.lambda_handler"
-
-  source_code_hash = chomp(data.aws_s3_bucket_object.visitorsapp_function_hash.body)
-
-  s3_bucket = "garrettleber-tf-backend"
-  s3_key    = "prod/lambda-zips/lambda_function_payload.zip"
-
-  runtime = "python3.8"
-  depends_on = [
-    aws_dynamodb_table.visitors_app_table,
-  ]
-
-  environment {
-    variables = {
-      VISITORS_TABLE = "Visitors"
-    }
-  }
+resource "aws_acm_certificate" "website_certificate" {
+  domain_name       = local.site_domain
+  validation_method = "DNS"
 
   lifecycle {
-    ignore_changes = [
-      last_modified,
-      qualified_arn,
-      version
-    ]
-  }
-}
-
-resource "aws_api_gateway_rest_api" "visitors_rest_api" {
-  name        = "visitors_rest_api"
-  description = "REST API for visitor count"
-}
-
-resource "aws_api_gateway_resource" "visitors_app_proxy" {
-  rest_api_id = aws_api_gateway_rest_api.visitors_rest_api.id
-  parent_id   = aws_api_gateway_rest_api.visitors_rest_api.root_resource_id
-  path_part   = "getcount"
-}
-
-resource "aws_api_gateway_method" "visitors_gateway_proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.visitors_rest_api.id
-  resource_id   = aws_api_gateway_resource.visitors_app_proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "visitors_app_lambda" {
-  rest_api_id = aws_api_gateway_rest_api.visitors_rest_api.id
-  resource_id = aws_api_gateway_method.visitors_gateway_proxy.resource_id
-  http_method = aws_api_gateway_method.visitors_gateway_proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.visitorsapp.invoke_arn
-  depends_on = [
-    aws_lambda_function.visitorsapp,
-  ]
-}
-
-resource "aws_api_gateway_deployment" "visitors_app_api_gateway_deploy" {
-  depends_on = [
-    aws_api_gateway_integration.visitors_app_lambda
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.visitors_rest_api.id
-  stage_name  = "prod"
-}
-
-resource "aws_lambda_permission" "visitors_apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.visitorsapp.function_name
-  principal     = "apigateway.amazonaws.com"
-
-
-  source_arn = "${aws_api_gateway_rest_api.visitors_rest_api.execution_arn}/*/*"
-}
-
-
-resource "aws_dynamodb_table" "visitors_app_table" {
-  name           = "Visitors"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "website"
-
-  attribute {
-    name = "website"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Visitors"
-    Environment = "production"
+    create_before_destroy = true
   }
 }
